9a47e4964f0042771512ddd70e5c8f22044761de
Iktaj singh <isigh145@myseneca.ca>
2022-11-17 23:56:37 -0500

    initial commit

--- blog-service.js
+++ blog-service.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const { gte } = Sequelize.Op;
 
-var sequelize = new Sequelize('oaepzsfp', 'oaepzsfp', 'rsYoIjW3vh0NRMuY_YzwTxa54i1M4orJ', {
-    host: 'abul.db.elephantsql.com',
+var sequelize = new Sequelize("oaepzsfp", "oaepzsfp", "rsYoIjW3vh0NRMuY_YzwTxa54i1M4orJ", {
+    host: "abul.db.elephantsql.com",
     dialect: 'postgres',
     port: 5432,
     dialectOptions: {
